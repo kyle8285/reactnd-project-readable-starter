1ca714b0caaeb2257fd55c7a5e94d5eed95e951a
kyle8285 <kylelussier3@gmail.com>
2017-11-26 22:58:36 -0500

    WIP; Very basic style changes

--- frontend/src/components/Post.js
+++ frontend/src/components/Post.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import MdEdit from 'react-icons/lib/md/edit';
 
 import VoteScore from './VoteScore';
 
 const Post = ({post}) => (
   <li>
-    <h3>{post.title}</h3>
-    <Link to={`/post/${post.id}/edit`}>Edit Post</Link>
-    <h4>{post.author}</h4>
+    <h3 className='title'>{post.title}</h3>
+    <Link to={`/post/${post.id}/edit`}><MdEdit/> Edit</Link>
+    <p>{new Date(post.timestamp).toString()} by {post.author}</p>
     <ul>
       <li>Category: {post.category}</li>
       <li>
         VoteScore: {post.voteScore}
         <VoteScore post={post}/>
       </li>
-      <li>Created: {new Date(post.timestamp).toString()}</li>
     </ul>
   </li>
 )
